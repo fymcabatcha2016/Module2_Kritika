5963fbb447cafddec99097279cf86f928db13594
Kritika <41277314+fymcabatcha2016@users.noreply.github.com>
2018-07-27 08:51:42 +0530

    String interpolation

--- Kritika/src/app/server/server.component.ts
+++ Kritika/src/app/server/server.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { server } from './server';
 
 @Component({
   selector: 'app-server',
---
+++ Kritika/src/app/server/server.ts
@@ -0,0 +1,11 @@
+export class server{
+    public connectionStaus;
+    constructor(){
+        this.connectionStaus="Not Connected";
+    }
+    public connect()
+    {
+        this.connectionStaus="Connected - task 6: Method calling";
+    }
+
+}
