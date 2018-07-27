ec12ac5224f22b0587726c3c7cdbd9d49592a76e
Kritika <41277314+fymcabatcha2016@users.noreply.github.com>
2018-07-27 09:50:36 +0530

    ng ifelse, ng for, ng style directory

--- Kritika/src/app/server/server.component.ts
+++ Kritika/src/app/server/server.component.ts
@@ -1,16 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 import { server } from './server';
-
+import {FormsModule} from '@angular/forms';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.css']
+  styleUrls: ['./server.component.css'],
+  providers:[server]
 })
 export class ServerComponent implements OnInit {
 
-  constructor() { }
+  public userlist=["abc","def","efh"];
+  public title ='Server Component - task 5 String interpolation';
+  public servername= "Server 1";
+  public servercreated=false;
+  public servercreationstatus = "No Server Created";
+  public id:number=1001;
+  public serverstatus1:string='offline';
 
+  constructor(public ServerObj:server) {
+    this.serverstatus1 = Math.random() > 0.5 ? 'online' : 'offline'
+   }
+   public getserverstatus(){
+    return this.serverstatus1;
+    }
+  public getcolor(){
+    return this.serverstatus1 === 'online' ? 'green' : 'red';
+  }
+  
+   public oncreateserver()
+   {
+     this.servercreated=true;
+     this.servercreationstatus="Server created task ngif ngifelse";
+   }
   ngOnInit() {
   }
-title ='Server Component - task 5 String interpolation';
 }
--- Kritika/src/app/server/server.ts
+++ Kritika/src/app/server/server.ts
@@ -1,11 +1,12 @@
 export class server{
     public connectionStaus;
+    
     constructor(){
         this.connectionStaus="Not Connected";
     }
     public connect()
     {
-        this.connectionStaus="Connected - task 6: Method calling";
+        this.connectionStaus="Connected - task 6 and 7 Method calling";
     }
 
 }
